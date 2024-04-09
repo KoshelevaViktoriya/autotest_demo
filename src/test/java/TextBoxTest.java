687fd754cc767cc109426d9b27bc1e8596b8bc3b
Viktoriya Kosheleva <viktoriyanov63@gmail.com>
2024-04-09 17:23:42 +0400

    added new test

---
+++ src/test/java/TextBoxTest.java
@@ -0,0 +1,29 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTest {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize= "1920*1080";
+        Configuration.baseUrl= "https://demoqa.com/";
+    }
+
+
+    //https://demoqa.com/text-box
+    @Test
+    void fillFormTest() {
+        open("text-box");
+        $("[id=userName]").setValue("Ivan Ivanov"); //id=#
+        $("#userEmail").setValue("eee@mail.com");
+        $("#currentAddress").setValue("Pushkina street 7");
+        $("#permanentAddress").setValue("Lesnaya 10");
+        $("#submit").click();
+
+        $("#output").shouldHave(text("Ivan Ivanov"), text("eee@mail.com"), text("Pushkina street 7"), text("Lesnaya 10"));
+    }
+}
