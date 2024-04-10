7551ea167dc05a55b8cbe9c0464b2ee6629e19e9
Viktoriya Kosheleva <viktoriyanov63@gmail.com>
2024-04-10 18:12:25 +0400

    added happy 1-test

---
+++ src/test/java/HoverTest.java
@@ -0,0 +1,31 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+//1. На главной странице GitHub выберите меню Solutions -> Enterprize
+// с помощью команды hover для Solutions. Убедитесь что загрузилась нужная страница
+// (например что заголовок - "Build like the best."
+
+
+public class HoverTest {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    void choseElementWithHover() {
+        open("https://github.com/");
+        $(byText("Solutions")).hover();
+        $(byText("Enterprise")).click();
+        Configuration.timeout= 5000;
+        $("#hero-section-brand-heading").shouldHave(text("The AI-powered\n" +
+                "developer platform."));
+    }
+}
--- src/test/java/WikiSelenideTest.java
+++ src/test/java/WikiSelenideTest.java
@@ -27,10 +27,14 @@ public class WikiSelenideTest {
 
     @Test
     void searchPage() {
+        //  - Откройте страницу Selenide в Github
         open("https://github.com/selenide/selenide");
+        // - Перейдите в раздел Wiki проекта
         $("#wiki-tab").click();
+        // - Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
         $("#wiki-pages-filter").setValue("SoftAssertions");
         $(".filterable-active").shouldHave(text("SoftAssertions"));
+        // - Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
         $(byText("SoftAssertions")).click();
         $(".markdown-body").shouldHave(text("Using JUnit5 extend test class:"));
     }
